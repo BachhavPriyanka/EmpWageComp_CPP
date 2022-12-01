41830b0d798ab76f27551e29b4cc21cadded2832
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-12-01 16:24:04 +0530

    Calculating Daily Employee wage

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -2,20 +2,30 @@
 using namespace std;
 #include<time.h>
 
-//UC1 : Check employee is present or absent
-int main()
+//UC2 : Calculate daily employee wage
+void empWage()
 {
-    cout<<"Welcome to employee wage computation program on master branch"<<endl;
     int isPresent = 1;
     int random;
+    int Emp_Wage_PerHour = 20;
+    int empHours;
     srand(time(0));
-    random = (rand() % 2);
+    random = (rand()%2);
     if(random == isPresent)
     {
-        cout<<"Employee is present";
+        cout<<"Employee is present"<<endl;
+        empHours = 8;
     }
     else{
-        cout<<"Employee is absent";
+        cout<<"Employee is absent"<<endl;
+        empHours = 0;
     }
+    int empWage = empHours * Emp_Wage_PerHour;
+    cout<<"Employee Hours : "<<empHours<<endl;
+    cout<<"Employee Wage : "<<empWage;
+}
+int main()
+{
+    empWage();
     return 0;
 }
