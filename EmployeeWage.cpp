c0bac68b78d4a5aa678f00ca97873e18f72467a7
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-12-01 16:30:22 +0530

    Calculating Employee doing part time or not andd calculating wage

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -2,27 +2,32 @@
 using namespace std;
 #include<time.h>
 
-//UC2 : Calculate daily employee wage
+//UC3 : Add part time employee & wage
 void empWage()
 {
-    int isPresent = 1;
-    int random;
-    int Emp_Wage_PerHour = 20;
-    int empHours;
     srand(time(0));
-    random = (rand()%2);
-    if(random == isPresent)
+    int random = rand() % 3;
+    int fullTime = 1;
+    int partTime = 2;
+    int empHours;
+    int Emp_Wage_PerHour = 20;
+    if(random == fullTime)
     {
-        cout<<"Employee is present"<<endl;
+        cout<<"Employee is doing Full Time"<<endl;
         empHours = 8;
+    }else if(random == partTime)
+    {
+        cout<<"Employee is doing Part Time"<<endl;
+        empHours = 4;
     }
     else{
-        cout<<"Employee is absent"<<endl;
+        cout<<"Employee is Absent"<<endl;
         empHours = 0;
     }
     int empWage = empHours * Emp_Wage_PerHour;
     cout<<"Employee Hours : "<<empHours<<endl;
     cout<<"Employee Wage : "<<empWage;
+    
 }
 int main()
 {
