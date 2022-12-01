a298070fadc0b2a831ff7e69c23fd16f10159c14
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-12-01 19:59:08 +0530

    Using Switch case

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -2,35 +2,40 @@
 using namespace std;
 #include<time.h>
 
-//UC3 : Add part time employee & wage
-void empWage()
-{
+//UC4 : Solved using switch case
+
+void empWageCal(int* empHrs, int* EMP_WAGE_PER_HOUR){
+		int empWage;
+	    empWage = ((*empHrs) * (*EMP_WAGE_PER_HOUR));
+	    cout<<"Emp Hours : "<<*empHrs<<endl;
+	    cout<<"Emp Wage : "<<empWage<<endl;
+}
+void empWage(){        
+	int empHrs, empWage;
     srand(time(0));
     int random = rand() % 3;
-    int fullTime = 1;
-    int partTime = 2;
-    int empHours;
-    int Emp_Wage_PerHour = 20;
-    if(random == fullTime)
-    {
-        cout<<"Employee is doing Full Time"<<endl;
-        empHours = 8;
-    }else if(random == partTime)
-    {
-        cout<<"Employee is doing Part Time"<<endl;
-        empHours = 4;
-    }
-    else{
-        cout<<"Employee is Absent"<<endl;
-        empHours = 0;
-    }
-    int empWage = empHours * Emp_Wage_PerHour;
-    cout<<"Employee Hours : "<<empHours<<endl;
-    cout<<"Employee Wage : "<<empWage;
-    
+	int EMP_WAGE_PER_HOUR = 20;
+	switch(random) {
+	    case 1:
+	        cout<<"Employee is Doing Full Time:"<<endl;
+	        empHrs = 8;
+	            break;
+	    case 2:
+	        cout<<"Employee is Doing Part Time:"<<endl;
+	        empHrs = 4;
+	            break;
+	    default:
+	        cout<<"Employee is Absent:"<<endl;
+	        empHrs = 0;
+	            break;
+	}
+	empWageCal(&empHrs, &EMP_WAGE_PER_HOUR);
 }
+
 int main()
 {
+    cout<<"Welcome to Employee Wage Computation"<<endl;
+    cout<<" Using switch case "<<endl;
     empWage();
-    return 0;
+
 }
