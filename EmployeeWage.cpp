0f0ca707d0f7fdcfeb9003a193719eafa5cdb841
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-12-01 16:15:08 +0530

    Checks whether Emp is present or absent

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
+#include<time.h>
 
+//UC1 : Check employee is present or absent
 int main()
 {
-    cout<<"Welcome to employee wage computation program on master branch"
+    cout<<"Welcome to employee wage computation program on master branch"<<endl;
+    int isPresent = 1;
+    int random;
+    srand(time(0));
+    random = (rand() % 2);
+    if(random == isPresent)
+    {
+        cout<<"Employee is present";
+    }
+    else{
+        cout<<"Employee is absent";
+    }
     return 0;
 }
